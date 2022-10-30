e75a5b2e54de829595f0496fe25ad8b95980f591
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2022-10-30 15:06:32 +0500

    Create: add-docker

--- src/main/java/ru/practicum/shareit/exception/BookingException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class BookingException extends RuntimeException {
-    public BookingException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/item/service/CommentService.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.item.service;
-
-import ru.practicum.shareit.item.dto.CommentDto;
-
-public interface CommentService {
-
-    CommentDto createComment(CommentDto commentDto, Long itemId, Long userId);
-}
